57a3c7516d51afccfac3263026a443f5988b8e50
Elizkoleary <eliz.k.oleary@gmail.com>
2019-10-23 20:31:49 -0500

    uncommented code

--- script2.js
+++ script2.js
@@ -1,29 +1,29 @@
-// var artistName = "Random Name";
-// var APIkey = "APIkey";
-// var artistID;
+var artistName = "Random Name";
+var APIkey = "APIkey";
+var artistID;
 
-// // Using this URL returns a response element that returns the artist ID, needed by other endpoints/AJAX calls.
-// var queryURL =
-//   "https://api.songkick.com/api/3.0/search/artists.json?apikey={" +
-//   APIkey +
-//   "}&query={" +
-//   artistName +
-//   "}";
+// Using this URL returns a response element that returns the artist ID, needed by other endpoints/AJAX calls.
+var queryURL =
+  "https://api.songkick.com/api/3.0/search/artists.json?apikey={" +
+  APIkey +
+  "}&query={" +
+  artistName +
+  "}";
 
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   artistID = response.resultsPage.results.artist[0].id;
-// });
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  artistID = response.resultsPage.results.artist[0].id;
+});
 
-// // Using this URL returns a response element that is used by the Crimometer API
-// var queryURL2 =
-//   "https://api.songkick.com/api/3.0/artists/{" +
-//   artistID +
-//   "}/calendar.json?apikey={" +
-//   APIkey +
-//   "}";
+// Using this URL returns a response element that is used by the Crimometer API
+var queryURL2 =
+  "https://api.songkick.com/api/3.0/artists/{" +
+  artistID +
+  "}/calendar.json?apikey={" +
+  APIkey +
+  "}";
 
 var lat
 var lon
