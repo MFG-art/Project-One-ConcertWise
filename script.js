b28161f6f5e37d27602b9f508f985605b62d6db3
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-11-02 09:43:53 -0500

    done

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-var APIKey = "jj9McEqabmQRWhL1nGOKGFZ1PmaWDWjy";
-var keyword = "Twenty One Pilots";
-keyword = encodeURI(keyword);
-var queryURL = "https://app.ticketmaster.com/discovery/v2/attractions?";
 var proxy = "https://chriscastle.com/proxy/index.php?:proxy:";
-var artistID;
 var latArray = [];
 var lngArray = [];
 var artistName;
@@ -13,39 +8,46 @@ var venueCity = [];
 var streetAddress = [];
 var eventURL = [];
 var endDateString = [];
+var eventState;
+var aggravatedAssault, theftFromMotorVehicle, drugViolations;
 
-// This AJAX call takes the user input and returns the artist ID
-
-$.ajax({
-  url: proxy + queryURL,
-  method: "GET",
-  dataType: "json",
-  data: "apikey=" + APIKey + "&keyword=" + keyword + "&locale=*",
-  success: function(data) {
-    if (data.page.totalElements > 0) {
-      // if there are more than one artist ID, select the first item
-      artistName = data._embedded.attractions[0].name;
-      artistID = data._embedded.attractions[0].id;
-      setTimeout(getEvents(), 500); // waits for 500 ms and then calls getEvents()
-    } else {
-      alert("Please enter another artist."); // replace this with MODAL!
+function getAttractionID(keyword) {
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/attractions?";
+  var APIKey = "jj9McEqabmQRWhL1nGOKGFZ1PmaWDWjy";
+  // This AJAX call takes the user input and returns the artist ID
+  $.ajax({
+    url: proxy + queryURL,
+    method: "GET",
+    dataType: "json",
+    data: "apikey=" + APIKey + "&keyword=" + keyword + "&locale=*",
+    success: function(data) {
+      if (data.page.totalElements > 0) {
+        console.log(data);
+        // if there are more than one artist ID, select the first item
+        artistName = data._embedded.attractions[0].name;
+        var artistID = data._embedded.attractions[0].id;
+        setTimeout(getEvents(artistID), 200); // waits for 200 ms and then calls getEvents()
+      } else {
+        alert("Please enter another artist."); // replace this with MODAL!
+      }
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.log("Error!");
     }
-  },
-  error: function(XMLHttpRequest, textStatus, errorThrown) {
-    console.log("Error!");
-  }
-});
+  });
+}
 
-var loops;
-
-function getEvents() {
+function getEvents(artistID) {
   var queryURL = "https://app.ticketmaster.com/discovery/v2/events?";
+  var APIKey = "jj9McEqabmQRWhL1nGOKGFZ1PmaWDWjy";
   $.ajax({
     url: proxy + queryURL,
     method: "GET",
     dataType: "json",
     data: "apikey=" + APIKey + "&attractionId=" + artistID + "&locale=*",
     success: function(data) {
+      console.log(data);
+      var loops;
       if (data._embedded.events.length < 3) {
         // this makes it so that only three or less events are displayed
         loops = data._embedded.events.length;
@@ -53,19 +55,14 @@ function getEvents() {
         loops = 3;
       }
       for (var i = 0; i < loops; i++) {
-        latArray[i] =
-          data._embedded.events[i]._embedded.venues[0].location.latitude;
-        lngArray[i] =
-          data._embedded.events[i]._embedded.venues[0].location.longitude;
         eventName[i] = data._embedded.events[i].name;
         venueName[i] = data._embedded.events[i]._embedded.venues[0].name;
         venueCity[i] = data._embedded.events[i]._embedded.venues[0].city;
-        console.log(data._embedded.events[i]._embedded.venues[0].city);
-        localStorage.setItem("city",data._embedded.events[i]._embedded.venues[0].city.name);
         streetAddress[i] =
           data._embedded.events[i]._embedded.venues[0].address.line1;
-        eventURL = data._embedded.events[i].url;
-        endDateString = data._embedded.events[i].dates.start.localDate;
+        eventURL[i] = data._embedded.events[i].url;
+        eventState =
+          data._embedded.events[i]._embedded.venues[0].state.stateCode;
       }
       crimeAPI();
     },
@@ -75,69 +72,89 @@ function getEvents() {
   });
 }
 
-var other = [];
-var otherNum = [];
-var theft = [];
-var theftNum = [];
-var assault = [];
-var assaultNum = [];
-
 function crimeAPI() {
-  // Crimometer api call
-
-  var endDateMoment = moment(endDateString[0], "YYYY-MM-DD");
-  var startDate = endDateMoment.subtract(1, "year");
   var crimeURL =
-    "https://private-anon-baa30e29b2-crimeometer.apiary-mock.com/v1/incidents/stats?";
-  let lat = latArray[0];
-  let lng = lngArray[0];
+    "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/aggravated-assault/offense/states/" +
+    eventState +
+    "/COUNT?";
+  var APIKey = "iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+
+  // Crimometer api call
   $.ajax({
     url: proxy + crimeURL,
     method: "GET",
     dataType: "json",
-    data:
-      "lat=" +
-      lat +
-      "&lon=" +
-      lng +
-      "&distance=10mi&datetime_ini=" +
-      startDate +
-      "&datetime_end=" +
-      endDateMoment +
-      ",&source=0",
+    data: "API_KEY=" + APIKey,
     success: function(data) {
-      console.log(venueCity[0]);
-      console.log(latArray[0]);
-      console.log(lngArray[0]);
       console.log(data);
-      other[0] = data[0].report_types[0].type;
-      otherNum[0] = data[0].report_types[0].count;
-      theft[0] = data[0].report_types[1].type;
-      theftNum[0] = data[0].report_types[1].count;
-      assault[0] = data[0].report_types[2].type;
-      assaultNum[0] = data[0].report_types[2].count;
+      var index = data.results.length - 1;
+      aggravatedAssault = data.results[index].offense_count;
+      crimeAPICall2();
     },
-    error: function() {
-      console.log("error with crime api loop");
+    error: function(data) {
+      console.log(data);
+      console.log("error");
     }
   });
-}
-$(".submit").on("click", function(event) {
-  event.preventDefault();
-  var artistList = JSON.parse(localStorage.getItem("artists"));
-  if (!artistList) {
-    artistList = [];
+
+  function crimeAPICall2() {
+    var crimeURL =
+      "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/theft-from-motor-vehicle/offense/states/" +
+      eventState +
+      "/COUNT?";
+    var APIKey = "iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+
+    $.ajax({
+      url: proxy + crimeURL,
+      method: "GET",
+      dataType: "json",
+      data: "API_KEY=" + APIKey,
+      success: function(data) {
+        console.log(data);
+        var index = data.results.length - 1;
+        theftFromMotorVehicle = data.results[index].offense_count;
+        crimeAPICall3();
+      },
+      error: function(data) {
+        console.log(data);
+        console.log("error");
+      }
+    });
   }
-  artistList.push({ keyword });
-  localStorage.setItem("artists", JSON.stringify(artistList));
-  
-  // $(artistName).append("#newArtist");
+
+  function crimeAPICall3() {
+    var crimeURL =
+      "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/drug-violations/offense/states/" +
+      eventState +
+      "/COUNT?";
+    var APIKey = "iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+
+    $.ajax({
+      url: proxy + crimeURL,
+      method: "GET",
+      dataType: "json",
+      data: "API_KEY=" + APIKey,
+      success: function(data) {
+        console.log(data);
+        var index = data.results.length - 1;
+        drugViolations = data.results[index].offense_count;
+        displayStuff();
+      },
+      error: function(data) {
+        console.log(data);
+        console.log("error");
+      }
+    });
+  }
+}
+
+function displayStuff() {
   $("#newArtist").append(artistName);
   var ticketmaster = $("<a>");
-  $(ticketmaster).html("Buy Tickets Here").attr('href', eventURL);
+  $(ticketmaster)
+    .html("Buy Tickets Here")
+    .attr("href", eventURL[0]);
   $("#tickets").append(ticketmaster);
-
-
 
   Highcharts.chart("container", {
     chart: {
@@ -146,7 +163,7 @@ $(".submit").on("click", function(event) {
       plotShadow: false
     },
     title: {
-      text: "Crimes<br>" + localStorage.getItem("city") + "<br>Total 1,112",
+      text: "Crimes",
       align: "center",
       verticalAlign: "middle",
       y: 60
@@ -175,8 +192,22 @@ $(".submit").on("click", function(event) {
         type: "pie",
         name: "Crime Percentage",
         innerSize: "50%",
-        data: [['Other', 576], ['Theft', 334], ['Assault', 202]]
+        data: [
+          ["Aggrevated Assault", aggravatedAssault],
+          ["Theft from Vehicle", theftFromMotorVehicle],
+          ["Drug Violation", drugViolations]
+        ]
       }
     ]
   });
+}
+
+$(".submit").on("click", function(event) {
+  event.preventDefault();
+
+  keyword = $("#myinput").val();
+  keyword = keyword.trim();
+  keyword = encodeURI(keyword);
+
+  getAttractionID(keyword);
 });
